40406d1aecbb44d7dbfda2a97581ad9232e1ddd9
Alex <alexlsouthall@yahoo.com>
2022-12-24 18:37:19 +0000

    Specified language, added a limti of news stories, non-hard coded subject added

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import requests
 from send_email import send_mail
 
 
+topic = "tesla"
+
 api_key = "c1260aba486b4afb8a1a214bfcda77c6"
-url = "https://newsapi.org/v2/everything?q=tesla&"\
-      "from=2022-11-22&sortBy   =publishedAt&apiKey=c1260aba486b4afb8a1a214bfcda77c6"
+url = "https://newsapi.org/v2/everything?"\
+    f"q='{topic}'&"\
+    "from=2022-11-22&sortBy"\
+    "=publishedAt&apiKey=c1260aba486b4afb8a1a214bfcda77c6&"\
+    "language=eng"
 
 r = requests.get(url)
 
@@ -13,8 +18,8 @@ email_cont = f"""\
 Subject: Today's news
 
 """
-for a in content["articles"]:
-    Title = (a["title"])
+for a in content["articles"][:20]:
+    Title = (a["title"])    
     source = (a["url"])
     Author = (a["source"]["Name"])
     news_story = f"""{Title}
