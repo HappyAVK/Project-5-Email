3cddd52bafea4d7e1c1486f8f1cae28f9dc67a30
Alex <alexlsouthall@yahoo.com>
2023-01-10 17:38:44 +0000

    Added Environment key for email sending, due to the personal nature of this project

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 from send_email import send_mail
-
+import time
 
 topic = "tesla"
-
+date = time.strftime("%Y-%m-%d")
 api_key = "c1260aba486b4afb8a1a214bfcda77c6"
 url = "https://newsapi.org/v2/everything?"\
-    f"q='{topic}'&"\
-    "from=2022-11-22&sortBy"\
-    "=publishedAt&apiKey=c1260aba486b4afb8a1a214bfcda77c6&"\
-    "language=eng"
+    f"q={topic}&"\
+    f"from={date}&sortBy=publishedAt"\
+    "&apiKey=c1260aba486b4afb8a1a214bfcda77c6&"\
+    "language=en"
 
 r = requests.get(url)
 
@@ -19,7 +19,7 @@ Subject: Today's news
 
 """
 for a in content["articles"][:20]:
-    Title = (a["title"])    
+    Title = (a["title"])
     source = (a["url"])
     Author = (a["source"]["Name"])
     news_story = f"""{Title}
--- send_email.py
+++ send_email.py
@@ -1,12 +1,12 @@
 import smtplib, ssl
 import os
 
-
+key = os.getenv('PyEmailKey')
 def send_mail(message):
     host = "smtp.gmail.com"
     port = 465
     username = "allalexandersouth@gmail.com"
-    password = "peoeuvljrzdfdyds"
+    password = os.getenv("Python_Email_Key")
     password1 = password
     receiver = "allalexandersouth@gmail.com"
     contexti = ssl.create_default_context()
@@ -14,3 +14,8 @@ def send_mail(message):
     with smtplib.SMTP_SSL(host, port, context=contexti) as server:
         server.login(username, password1)
         server.sendmail(username, receiver, message)
+
+
+if __name__ == "__main__":
+    print(key)
+    print (os.environ)
